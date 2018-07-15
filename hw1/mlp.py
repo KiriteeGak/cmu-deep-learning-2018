67c592230de3a2ff7c22fcaa5a866dfe1e4f9024
KiriteeGak <kiritee.gandamalla@gmail.com>
2018-07-16 00:49:26 +0530

    added momentum

--- hw1/mlp.py
+++ hw1/mlp.py
@@ -3,7 +3,13 @@ from math import tanh
 
 
 class MultiLayerPerceptron(object):
-    def __init__(self, layers, size, activations, iterations=5, learning_rate=0.01, threshold_error=0.01):
+    def __init__(self, layers, size,
+                 activations,
+                 iterations=5,
+                 learning_rate=0.01,
+                 threshold_error=0.01,
+                 momentum=None,
+                 beta=None):
         self.input_size = None
         self.output_size = None
         self.layers = layers
@@ -16,23 +22,37 @@ class MultiLayerPerceptron(object):
         self.model = {}
         self.learning_rate = learning_rate
         self.threshold_error = threshold_error
+        self.momentum = momentum
+        self._velocities = []
+        self.beta = beta
 
-    def _initialise_weights(self):
+    def _initialise_weights(self, momentum=False):
         weights = []
         for _ in range(0, len(self.size) + 1):
             if not _:
-                weights.append(np.random.rand(self.input_size, self.size[_]) -
-                               np.random.rand(self.input_size, self.size[_]))
+                if not momentum:
+                    weights.append(np.random.rand(self.input_size, self.size[_]) -
+                                   np.random.rand(self.input_size, self.size[_]))
+                else:
+                    self._velocities.append(np.zeros((self.input_size, self.size[_])))
             elif _ == len(self.size):
-                weights.append(np.random.rand(self.size[-1], self.output_size) -
-                               np.random.rand(self.size[-1], self.output_size))
+                if not momentum:
+                    weights.append(np.random.rand(self.size[-1], self.output_size) -
+                                   np.random.rand(self.size[-1], self.output_size))
+                else:
+                    self._velocities.append(np.zeros((self.size[-1], self.output_size)))
             else:
-                weights.append(np.random.rand(self.size[_ - 1], self.size[_]) -
-                               np.random.rand(self.size[_ - 1], self.size[_]))
+                if not momentum:
+                    weights.append(np.random.rand(self.size[_ - 1], self.size[_]) -
+                                   np.random.rand(self.size[_ - 1], self.size[_]))
+                else:
+                    self._velocities.append(np.zeros((self.size[_ - 1], self.size[_])))
         return weights
 
-    def _initialise_bias(self):
-        return [np.ones(_.shape[1]) for _ in self.weights]
+    def _initialise_bias(self, momentum=False):
+        if not momentum:
+            return [np.ones(_.shape[1]) for _ in self.weights]
+        return [np.zeros(_.shape[1]) for _ in self.weights]
 
     def fit(self, x, y):
         self.input_size = x[0].shape[0]
@@ -43,19 +63,44 @@ class MultiLayerPerceptron(object):
         self.model['weights2'] = self.weights[1]
         self.model['bias1'] = self.bias[0]
         self.model['bias2'] = self.bias[1]
+        self._generate_momentum()
+        self.model['momentum_weights1'] = self._velocities[0]
+        self.model['momentum_weights2'] = self._velocities[1]
+        self.model['momentum_bias1'] = self._velocities[2][0]
+        self.model['momentum_bias2'] = self._velocities[2][1]
         iterations_occured = 0
         _error = np.inf
 
-        while iterations_occured < self.iterations or _error < self.threshold_error:
+        while iterations_occured < self.iterations and _error > self.threshold_error:
+            X, y = make_datasets(size=1)
             self.layer_outputs = self.forward_propagation(x=X)
             weights_bias = self.back_propagation(X=x,
                                                  y=self.layer_outputs[-1],
                                                  output=y,
                                                  a2=self.layer_outputs[1])
-            self.model['weights1'] -= self.learning_rate * weights_bias[0][0]
-            self.model['bias1'] -= self.learning_rate * weights_bias[0][1]
-            self.model['weights2'] -= self.learning_rate * weights_bias[1][0]
-            self.model['bias2'] -= self.learning_rate * weights_bias[1][1]
+            if not self.momentum:
+                self.model['weights1'] -= self.learning_rate * weights_bias[0][0]
+                self.model['bias1'] -= self.learning_rate * weights_bias[0][1]
+                self.model['weights2'] -= self.learning_rate * weights_bias[1][0]
+                self.model['bias2'] -= self.learning_rate * weights_bias[1][1]
+            else:
+                self.model['weights1'] -= (self.beta * self.model['momentum_weights1'] +
+                                           (1 - self.beta) * weights_bias[0][0]) * self.learning_rate
+                self.model['bias1'] -= (self.beta * self.model['momentum_bias1'] +
+                                        (1 - self.beta) * weights_bias[0][1]) * self.learning_rate
+                self.model['weights2'] -= (self.beta * self.model['momentum_weights2'] +
+                                           (1 - self.beta) * weights_bias[1][0]) * self.learning_rate
+                self.model['bias2'] -= (self.beta * self.model['momentum_bias2'] +
+                                        (1 - self.beta) * weights_bias[1][1]) * self.learning_rate
+                self.model['momentum_weights1'] = self.beta * self.model['momentum_weights1'] + \
+                                                        (1 - self.beta) * weights_bias[0][0]
+                self.model['momentum_weights2'] = self.beta * self.model['momentum_weights2'] + \
+                                                        (1 - self.beta) * weights_bias[1][0]
+                self.model['momentum_bias1'] = (self.beta * self.model['momentum_bias1'] +
+                                                      (1 - self.beta) * weights_bias[0][1])
+                self.model['momentum_bias2'] = (self.beta * self.model['momentum_bias2'] +
+                                                      (1 - self.beta) * weights_bias[1][1])
+
             self.weights = [self.model['weights1'], self.model['weights2']]
             self.bias = [self.model['bias1'], self.model['bias2']]
 
@@ -78,8 +123,8 @@ class MultiLayerPerceptron(object):
         db1 = np.sum(delta1, axis=0)
         # Add regularization terms
         # dW3 += reg_lambda * model['W3']
-        dW2 += reg_lambda * self.weights[1]
-        dW1 += reg_lambda * self.weights[0]
+        # dW2 += reg_lambda * self.weights[1]
+        # dW1 += reg_lambda * self.weights[0]
         return (dW1, db1), (dW2, db2)
 
     def _activation_derivative(self, activation, activation_type=None, derivative=False):
@@ -117,6 +162,10 @@ class MultiLayerPerceptron(object):
             x = z
         return layer_outputs
 
+    def _generate_momentum(self):
+        self._initialise_weights(momentum=True)
+        self._velocities.append(self._initialise_bias(momentum=True))
+
     @classmethod
     def error_(cls, output, actual, type_=None, epsilon=1e-12, derivative=False):
         if type_ == "crossentropy":
@@ -136,9 +185,9 @@ class MultiLayerPerceptron(object):
                          zip(actual, output)])
 
 
-def make_datasets():
+def make_datasets(size=100):
     from sklearn import datasets
-    X, y = datasets.make_moons(100, noise=0.10)
+    X, y = datasets.make_moons(size, noise=0.10)
     y_ = list()
     for _ in y:
         base_ = np.array([0, 0])
@@ -148,11 +197,13 @@ def make_datasets():
 
 
 if __name__ == '__main__':
-    X, y = make_datasets()
+    X, y = make_datasets(size=1)
     s = MultiLayerPerceptron(layers=1,
-                             size=[20],
+                             size=[3],
                              activations=('relu', 'softmax'),
-                             iterations=50,
+                             iterations=200,
                              learning_rate=0.01,
-                             threshold_error=1)
+                             threshold_error=0.15,
+                             momentum=True,
+                             beta=0.9)
     s.fit(x=X, y=y)
