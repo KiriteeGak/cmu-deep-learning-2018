309f17ccf8e6125fd0b19fc2bf116702cce2ed71
KiriteeKarunya <kiritee@tatrasdata.com>
2018-07-21 11:48:09 +0530

    hw2 updates/loading p2

--- hw2/cnn_cifar10.py
+++ hw2/cnn_cifar10.py
@@ -1,5 +1,5 @@
-from keras.models import Sequential
-from keras.layers import Conv2D, Dropout, Dense, Flatten, Activation
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, Dropout, Dense, Flatten, Activation, GlobalAveragePooling2D, AveragePooling2D
 from keras.utils.np_utils import to_categorical
 from keras.metrics import categorical_accuracy
 
@@ -8,46 +8,50 @@ from scipy.ndimage import rotate
 
 
 class PreProcessing(object):
-    def __init__(self):
+    def __init__(self, folder_name, file_ending, rotate_=True):
+        self.folder_name = folder_name
+        self.file_ending = file_ending
+        self.rotate_ = rotate_
         self.X, self.Y = self.load_data()
         self.Y = to_categorical(self.Y, num_classes=10)
 
-    @staticmethod
-    def load_data():
+    def load_data(self):
         import os
         import pickle
         import numpy as np
 
         data = []
         labels = []
-        files = [f for f in os.listdir("cifar-10-batches-py") if "data_batch" in f]
+        files = [f for f in os.listdir(self.folder_name) if self.file_ending in f]
 
         for f_name in files:
-            with open("cifar-10-batches-py/{}".format(f_name), 'rb') as fo:
+            with open("{}/{}".format(self.folder_name, f_name), 'rb') as fo:
                 dict_ = pickle.load(fo, encoding='bytes')
                 data.append(dict_[b'data'])
                 labels.append(dict_[b'labels'])
         return np.concatenate(data), np.concatenate(labels)
 
     def sample_zero_mean(self):
-        return np.apply_along_axis(lambda arr: (arr-arr.mean())/arr.std(), -1, self.X[:2])
+        return np.apply_along_axis(lambda arr: (arr - arr.mean()) / arr.std(), -1, self.X[:2])
 
-    def feature_zero_mean(self, epsilon=10**-5):
+    def feature_zero_mean(self, epsilon=10 ** -5):
         x_ = np.zeros(self.X.shape)
         _y = np.argmax(self.Y, axis=1)
 
         for label_id in range(10):
             idx = np.where(_y == label_id)
             mean_, std_ = self.X[idx].mean(), self.X[idx].std()
-            x_[idx] = np.apply_along_axis(lambda a: (a-mean_)/(std_+epsilon), -1, self.X[idx])
+            x_[idx] = np.apply_along_axis(lambda a: (a - mean_) / (std_ + epsilon), -1, self.X[idx])
         self.X = x_
 
     # How is this going to be any more special than standard normalising ...
-    def global_contrast_normalisation(self, s=1, lambda_=10**-5, epsilon=0.1):
-        self.X = np.apply_along_axis(lambda a: s * (a-a.mean())/max(np.sqrt(lambda_ + np.mean(a ** 2)), epsilon),
+    def global_contrast_normalisation(self, s=1, lambda_=10, epsilon=10 ** -6):
+        self.X = np.apply_along_axis(lambda a: s * (a - a.mean()) / max(np.sqrt(lambda_ + np.mean(a ** 2)), epsilon),
                                      -1,
                                      self.X)
 
+    # Complete zca normalisation.
+    # Check https://stackoverflow.com/questions/31528800/how-to-implement-zca-whitening-python
     def zca_normalisation(self):
         pass
 
@@ -71,7 +75,10 @@ class PreProcessing(object):
         self.sample_zero_mean()
         self.feature_zero_mean()
         self.global_contrast_normalisation()
-        self.shuffle_and_rotate()
+        if self.rotate_:
+            self.shuffle_and_rotate()
+        else:
+            self._reshape()
         return self.X, self.Y
 
 
@@ -84,13 +91,19 @@ def cnn_architecture():
     3 × 3 conv. 192 ReLU
     3 × 3 conv. 192 ReLU
     3 × 3 conv. 192 ReLU with stride r = 2
+    3 × 3 conv. 192 ReLU
+    1 × 1 conv. 192 ReLU
+    1 × 1 conv. 10 ReLU
+    global averaging over 6 × 6 spatial dimensions
+    10 or 100-way softmax
     :return:
     """
-    # X, Y = PreProcessing().chain_pre_processes()
-
+    # X, Y = PreProcessing(folder_name="cifar-10-batches-py",
+    #                      file_ending="data_batch",
+    #                      rotate_=True).chain_pre_processes()
+    #
     # np.save("data/train_data_transformed", X)
     # np.save("data/train_labels_transformed", Y)
-    # exit()
 
     X = np.load("data/train_data_transformed.npy")
     Y = np.load("data/train_labels_transformed.npy")
@@ -101,33 +114,52 @@ def cnn_architecture():
                      input_shape=(32, 32, 3),
                      border_mode='same',
                      data_format='channels_last'))
+    model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(Conv2D(filters=96, kernel_size=(3, 3), border_mode='same'))
+    model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(Conv2D(filters=96, kernel_size=(3, 3), strides=2))
+    model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(Conv2D(filters=192, kernel_size=(3, 3), border_mode='same'))
+    model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(Conv2D(filters=192, kernel_size=(3, 3), border_mode='same'))
+    model.add(Dropout(0.2))
     model.add(Activation('relu'))
     model.add(Conv2D(filters=192, kernel_size=(3, 3), strides=2))
+    model.add(Dropout(0.2))
     model.add(Activation('relu'))
+    model.add(Conv2D(filters=192, kernel_size=(3, 3), border_mode='same'))
+    model.add(Dropout(0.2))
+    model.add(Activation('relu'))
+    model.add(Conv2D(filters=192, kernel_size=(1, 1), border_mode='same'))
+    model.add(Dropout(0.2))
+    model.add(Activation('relu'))
+    model.add(Conv2D(filters=10, kernel_size=(1, 1), border_mode='same'))
+    model.add(Dropout(0.2))
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(6, 6)))
 
     model.add(Flatten())
-    model.add(Dense(units=1000, init='normal'))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(units=1000, init='normal'))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    model.add(Dense(10, init='normal'))
+    model.add(Dense(units=10, init='normal'))
     model.add(Activation('softmax'))
 
-    model.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=[categorical_accuracy])
-    model.fit(X, Y, epochs=10, batch_size=32)
+    # model.add(Dense(units=1000, init='normal'))
+    # model.add(Activation('relu'))
+    # model.add(Dropout(0.2))
+    # model.add(Dense(10, init='normal'))
 
-    model.save("models/cifar_10_1000_1000_10_10.h5py")
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=[categorical_accuracy])
+    model.fit(X, Y, epochs=1, validation_split=0.2)
+    model.save("models/cifar_10_1000_1000_10_1.h5py")
 
 
-# cnn_architecture()
+cnn_architecture()
+# model = load_model("models/cifar_10_1000_1000_10_1.h5py")
+# x_test, y_test = PreProcessing(folder_name="cifar-10-batches-py",
+#                                file_ending="test_batch",
+#                                rotate_=False).chain_pre_processes()
 
+# print(np.count_nonzero(np.sum(model.predict(x_test)*y_test, axis=1)) * 100 / 10000)
---
+++ hw2/phoneme_prediction_cnn.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+TRAIN_DATA_RAW = np.load("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/hw2/data/train-features.npy")
+TRAIN_DATA_LABELS = np.load("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/hw2/data/train-labels.npy")
+
+
+# Architecture - segmented
