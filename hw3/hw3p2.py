5157b6b18581ccc584d0f32f7fb471dda00adaf3
KiriteeKarunya <kiritee@tatrasdata.com>
2018-08-17 09:30:33 +0530

    test for modified hw3p2

--- hw3/hw3p2.py
+++ hw3/hw3p2.py
@@ -1,9 +1,11 @@
 import numpy as np
 
+from keras.backend.tensorflow_backend import eval
 from keras.models import Sequential
 from keras.layers import LSTM, TimeDistributed, Dense
 from keras.layers.core import Activation
 from keras.losses import categorical_crossentropy
+from keras.models import load_model
 
 from hw2.masked_batch_1 import bucketize_points
 
@@ -19,26 +21,48 @@ def lstm_architecture(size):
     return model
 
 
-def make_data():
+def make_data(data_size=None, train_data_raw=None, train_data_labels=None):
+    for st in range(0, data_size, 32):
+        padded_sequences, padded_labels = [], []
+        for i, _ in enumerate(train_data_raw[st:st+32]):
+            seq_, label = bucketize_points(_, to_one=max_size, masks=False, category=train_data_labels[i])
+            padded_sequences.append(seq_), padded_labels.append(label)
+        yield np.array(padded_sequences), np.array(padded_labels, dtype='float32')
+
+
+if __name__ == '__main__':
     train_data_raw = np.load("/home/kiriteegak/Desktop/"
                              "github-general/cmu-deep-learning-2018/"
                              "resources/HW1P2/train.npy",
                              encoding="bytes")
-    train_data_labels = np.load("/home/kiriteegak/Desktop/"
-                                "github-general/cmu-deep-learning-2018/"
-                                "resources/HW1P2/train_labels.npy",
-                                encoding="bytes")
+    # train_data_labels = np.load("/home/kiriteegak/Desktop/"
+    #                             "github-general/cmu-deep-learning-2018/"
+    #                             "resources/HW1P2/train_labels.npy",
+    #                             encoding="bytes")
     max_size = max([_.shape[0] for _ in train_data_raw])
-    padded_sequences, padded_labels = [], []
-    for i, _ in enumerate(train_data_raw):
-        seq_, label = bucketize_points(_, to_one=max_size, masks=False, category=train_data_labels[i])
-        padded_sequences.append(seq_), padded_labels.append(label)
-    return np.array(padded_sequences), np.array(padded_labels), max_size
+    # data_size = train_data_labels.shape[0]
+    #
+    # model_ = lstm_architecture(max_size)
+    # model_ = load_model("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/"
+    #                     "hw3/models/1_layer_lstm_p2_modified_epoch_1")
+    # for _ in range(8):
+    #     model_.fit_generator(make_data(),
+    #                          shuffle=True,
+    #                          steps_per_epoch=int(train_data_labels.shape[0]/32))
+    #     model_.save("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/"
+    #                 "hw3/models/1_layer_lstm_p2_modified_epoch_{}".format(_+2))
 
+    test_data_raw = np.load("/home/kiriteegak/Desktop/github-general/"
+                            "cmu-deep-learning-2018/hw3/data/dev.npy")
+    test_data_labels = np.load("/home/kiriteegak/Desktop/github-general/"
+                               "cmu-deep-learning-2018/hw3/data/dev_phonemes.npy")
 
-if __name__ == '__main__':
-    seq, labels, size_ = make_data()
-    np.save("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/hw3/data/hw2_modified/sequences.npy", seq)
-    np.save("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/hw3/data/hw2_modified/labels.npy", labels)
-    model_ = lstm_architecture(size_)
-    model_.fit(x=seq, y=labels, batch_size=32, shuffle=True, epochs=100)
+    model = load_model("/home/kiriteegak/Desktop/github-general/cmu-deep-learning-2018/"
+                       "hw3/models/1_layer_lstm_p2_modified_epoch_9")
+
+    acc = []
+    for d, labels in make_data(data_size=test_data_raw.shape[0],
+                               train_data_raw=test_data_raw,
+                               train_data_labels=test_data_labels):
+        acc.append(np.mean((labels * model.predict(d)).sum(axis=2).sum(axis=1)/max_size))
+    print("Average accuracy {}".format(np.mean(np.array(acc)) * 100))
